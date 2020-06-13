d87bd23793d7cd57092b555433ab3ffda04d41ca
William Grant <willmgrant@gmail.com>
2020-06-13 11:18:10 +0900

    updated latest sidebar icon, no favorites support, cleanup

--- src/app/latest/latest.component.ts
+++ src/app/latest/latest.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Router } from '@angular/router';
 import { PodcastService } from '../services/podcast.service';
-import { AudioService } from '../services/audio.service';
 import { FavouritesService } from '../services/favourites.service';
-// import { OfflineService } from '../services/offline.service'; // may need
 import { ToastService } from '../services/toast.service';
 import { Description } from '../pipes/description.pipe';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faInfoCircle, faTh, faList } from '@fortawesome/free-solid-svg-icons';
 import * as Store from 'electron-store';
 import * as parsePodcast from 'node-podcast-parser';
@@ -19,34 +15,19 @@ import * as parsePodcast from 'node-podcast-parser';
 })
 export class LatestComponent implements OnInit {
 	private store = new Store();
-	// public podcasts = [];
-	// public amount: number;
-	// public categories = [];
-	// public category: String;
-	// public regions = [];
-	// public region: String;
-	// public favs: string[];
 	public latest: Array<Object> = [];
 	public layout: string = "list";
 
-	public faHeart = faHeart;
 	public faInfoCircle = faInfoCircle;
 	public faTh = faTh;
 	public faList = faList;
 
-	constructor(private audio: AudioService,
-		// private route: ActivatedRoute,
-		// private router: Router,
-		private podcastService: PodcastService,
+	constructor(private podcastService: PodcastService,
 		private favService: FavouritesService,
-		// private offlineService: OfflineService,
 		private toast: ToastService,
 		private descriptionPipe: Description) { }
 
 	ngOnInit() {
-		// this.amount = 50;
-		// this.categories = this.podcastService.getCategories();
-		// this.regions = this.podcastService.getRegions();
 		this.layout = this.store.get("layout", "grid") as string;
 		this.favService.favourites.subscribe(value => {
 			const latest = [];
@@ -67,36 +48,11 @@ export class LatestComponent implements OnInit {
 				});
 			});
 			this.latest = latest;
-			console.log('latest', this.latest);
 		});
-
-		// //Listen for changes in URL parameters
-		// this.route.paramMap.subscribe(params => {
-		// 	this.region = params.get("region") || this.store.get("region", "us") as string;
-		// 	this.category = params.get("category") || "26";
-		// 	if (this.region && this.category) this.getPodcasts();
-		// })
 	}
-
-	// getPodcastsNavigation = () => {
-	// 	this.router.navigate(["/latest", { region: this.region, category: this.category }]);
-	// }
-
-	// getPodcasts = () => {
-	// 	this.podcastService.getLatestlist(this.region, this.category, this.amount).subscribe((data) => {
-	// 		this.podcasts = data['feed']['entry'];
-	// 	});
-	// }
-
-	// addPodcast(id): void {
-	// 	this.favService.addItunesFavourite(id);
-	// }
 
 	showDescription(title, description): void {
 		this.toast.modal(title, this.descriptionPipe.transform(description));
 	}
 
-	// isFavourite = (title) => {
-	// 	return this.favs.indexOf(title) !== -1;
-	// };
 }
